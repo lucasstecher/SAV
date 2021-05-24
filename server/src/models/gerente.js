8f9b0393f7c98c66b929422838a1e24921da89e6
Lucas.Stecher <lucasvieira0070@gmail.com>
2021-05-24 16:36:21 -0300

    configuração das outras models

--- server/src/models/gerente.js
+++ server/src/models/gerente.js
@@ -2,15 +2,19 @@ const Sequelize = require('sequelize');
 const connection = require('../database/database');
 
 const Gerente = connection.define('tb_gerente', {
-    nome:{
+    idt_gerente:{
+        type: Sequelize.STRING,
+        primaryKey: true
+    },
+    nome_gerente:{
         type: Sequelize.STRING,
         allowNull: false
     },
-    login:{
+    login_gerente:{
         type: Sequelize.STRING,
         allowNull: false
     },
-    senha:{
+    senha_gerente:{
         type: Sequelize.STRING,
         allowNull: false
     }
