60c8c77a601baf6f1e82c30eb0e51b5f9abcedbb
Lucas.Stecher <lucasvieira0070@gmail.com>
2021-05-25 19:46:03 -0300

    função login funcionando, adição de itens nas outras tables

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 // faz a requisição de express, algo semelhante ao #include em C ou import em py
 const express = require("express");
 const bodyParser = require("body-parser");
+// const session = require("express-session");
 const connection = require("./database/database");
 const Cliente = require("./models/Cliente");
 const Estoque = require("./models/Estoque");
@@ -27,6 +28,15 @@ app.use(bodyParser.json());
 
 // define o local dos statics
 app.use(express.static('../../'));
+
+// session para login
+/* app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json()); */
 
 
 // .get especifica o que o que acontece quando o navegador
@@ -66,13 +76,33 @@ app.listen(3000, () => {
 // está autenticando com valores pré definidos, editar para
 // funcionar procurando no banco de dados, usar parâmetro talvez? (segurança)
 // incluir alert ao errar login/senha
-app.post("/autenticar", (req, res) => {
+
+/* app.post("/autenticar", (req, res) => {
     if(req.body.usuario == 'gerente' && req.body.senha == 'gerente'){
         res.redirect('/menu');
     } else {
         res.redirect('/');
     }
-})
+}); */
+
+
+
+app.post('/autenticar', async (req, res) => {
+	var usuario = req.body.usuario;
+	var senha = req.body.senha;
+	if (usuario && senha) {
+        const [results] = await connection.query(`SELECT * FROM tb_gerente WHERE login_gerente = '${usuario}' AND senha_gerente = '${senha}'`);
+			if (results.length > 0) {
+				res.redirect('/menu');
+			} else {
+				res.redirect('/');
+			}
+            res.end();
+	}
+});
+
+
+
 
 app.post("/salvarcliente", (req,res) =>{
     var nome = req.body.nome;
@@ -88,4 +118,66 @@ app.post("/salvarcliente", (req,res) =>{
         console.log('Cliente adicionado!');
         res.redirect("/");
     })
-})
+});
+
+app.post("/salvarestoque", (req,res) =>{
+    var nome = req.body.nome;
+    var preco = req.body.preco;
+    var quantidade = req.body.quantidade;
+    var codigo = req.body.codigo;
+    Estoque.create({
+        nome_produto: nome,
+        preco_produto: preco,
+        quant_produto: quantidade,
+        codigo_produto: codigo
+    }).then(() => {
+        console.log('Produto adicionado!');
+        res.redirect("/");
+    })
+});
+
+app.post("/salvarfuncionario", (req,res) =>{
+    var nome = req.body.nome;
+    var login = req.body.login;
+    var senha = req.body.senha;
+    Funcionario.create({
+        nome_funcionario: nome,
+        login_funcionario: login,
+        senha_funcionario: senha
+    }).then(() => {
+        console.log('Funcionario adicionado!');
+        res.redirect("/");
+    })
+});
+
+app.post("/salvargerente", (req,res) =>{
+    var nome = req.body.nome;
+    var login = req.body.login;
+    var senha = req.body.senha;
+    Gerente.create({
+        nome_gerente: nome,
+        login_gerente: login,
+        senha_gerente: senha,
+    }).then(() => {
+        console.log('Gerente adicionado!');
+        res.redirect("/");
+    })
+});
+
+app.get("/gerente", (req, res) => {
+    res.sendFile('gerente.html', {root:'../../web/components'});
+});
+
+/*app.post("/salvarvenda", (req,res) =>{
+    var nome = req.body.nome;
+    var login = req.body.login;
+    var senha = req.body.senha;
+    Venda.create({
+        nome_gerente: nome,
+        login_gerente: login,
+        senha_gerente: senha,
+    }).then(() => {
+        console.log('Gerente adicionado!');
+        res.redirect("/");
+    })
+});*/
