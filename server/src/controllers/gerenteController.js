a01ef9b35ab629fd9b3be5dd3635502f3aa6ec54
Lucas.Stecher <lucasvieira0070@gmail.com>
2021-06-15 15:52:40 -0300

    add/edit gerente

--- server/src/controllers/gerenteController.js
+++ server/src/controllers/gerenteController.js
@@ -20,8 +20,41 @@ module.exports = {
             senha_gerente: senha,
         }).then(() => {
             console.log('Gerente adicionado!');
-            res.redirect("/gerente");
+            res.redirect("/gerentes");
         })
+    },
+    async delete (req,res){
+        Gerente.destroy({where: {'idt_gerente': req.params.id}}).then(()=>{
+            res.redirect('/gerentes')
+        }).catch((erro)=>{
+            res.send('Gerente não existe')
+        })
+    },
+    async edit(req, res){
+        Gerente.findOne({where: {'idt_gerente': req.params.id}}).then((gerente) => {
+            res.render("../../web/views/editgerente", {gerente: gerente})
+            
+        }).catch((erro) => {
+            res.send('Este funcionário não existe')
+        })
+    },
+    async editger(req,res){
+        Gerente.findOne({where: {'idt_gerente': req.body.id}}).then((gerente)=> {
+            gerente.nome_gerente = req.body.nome
+            gerente.login_gerente = req.body.login
+            gerente.senha_gerente = req.body.senha
+
+            gerente.save().then(()=>{
+                
+                res.redirect('/gerentes')
+            })
+
+        }).catch((err) => {
+            res.send('Houve um erro ao editar o gerente.')
+        })
+    }, 
+    async addger(req, res){
+        res.render('addgerente', {root:'../../web/views'});
     }
 }
 
--- server/src/routes/gerenteRoute.js
+++ server/src/routes/gerenteRoute.js
@@ -5,9 +5,9 @@ const controller = require('../controllers/gerenteController');
 
 router.get("/gerentes", controller.index);
 router.post("/salvargerente", controller.store);
-//router.get("/deletarger/:id", controller.delete);
-//router.get("/editarger/:id", controller.edit);
-//router.post("/editarger", controller.editgerente);
-//router.post("/addger", controller.addgerente);
+router.get("/deletarger/:id", controller.delete);
+router.get("/editarger/:id", controller.edit);
+router.post("/editarger", controller.editger);
+router.get("/addger", controller.addger);
 
 module.exports = router;
